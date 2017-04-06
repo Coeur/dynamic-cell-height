56e1a23a02ded76f5bbf3660a4e08805722bca9f
Antoine Cœur <antoine.coeur@ef.com>
2017-04-06 15:43:27 +0800

    the goal is to achieve constraint changes in `cellForRowAt` http://stackoverflow.com/questions/43225191/activating-nslayoutconstraint-in-cellforrowat-is-not-rendering-and-causes-una

--- dynamicCellHeight/ViewController.swift
+++ dynamicCellHeight/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.estimatedRowHeight = 10
+            tableView.rowHeight = UITableViewAutomaticDimension
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
     }
-
-
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+    }
 }
 
+class MyTableViewCell: UITableViewCell {
+    @IBOutlet var aConstraint: NSLayoutConstraint!
+    @IBOutlet var bConstraint: NSLayoutConstraint!
+}
+
+extension ViewController: UITableViewDelegate {
+    /*
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    }
+    */
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyTableViewCell", for: indexPath) as! MyTableViewCell
+        cell.contentView.translatesAutoresizingMaskIntoConstraints = false
+        if true {
+            // the goal is to achieve constraint changes in `cellForRowAt`
+            NSLayoutConstraint.deactivate([cell.aConstraint])
+            NSLayoutConstraint.activate([cell.bConstraint])
+        } else {
+            NSLayoutConstraint.deactivate([cell.bConstraint])
+            NSLayoutConstraint.activate([cell.aConstraint])
+        }
+        return cell
+    }
+}
